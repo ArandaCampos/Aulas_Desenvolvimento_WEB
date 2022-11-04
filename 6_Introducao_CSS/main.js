ebc67a820daaacdf454e0c21985ddc3bc105d422
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-11-04 17:39:20 -0300

    Upgrade Aula 6

--- 6_Introducao_CSS/main.js
+++ 6_Introducao_CSS/main.js
@@ -1,67 +1,31 @@
 const context = document.querySelector(".carousel")
 const pageOne = document.querySelector(".page-1")
 const pageTwo = document.querySelector(".page-2")
-// const pageThree = document.querySelector(".page-3")
-
 const on = '#45C4B0'
 const off = 'rgb(245, 245, 245)'
-
 var position = 1
 
-var altura = window.screen.height;
-var largura = window.screen.width;
-
-console.log(altura, largura)
-pageOne.style.backgroundColor = on
-context.style.animationName = 'twoToOne'
-            
-pageOne.addEventListener("click", function(event) {
+const init = () => {
     pageOne.style.backgroundColor = on
-    switch (position){
-        case 1:
-            break
-        case 2:
-            pageTwo.style.backgroundColor = off
-            context.style.animationName = 'twoToOne'
-            break
-        // case 3:
-        //     pageThree.style.backgroundColor = off
-        //     context.style.animationName = 'threeToOne'
-        //     break
-    }    
-    position = 1
-})
+    context.style.animationName = 'twoToOne'
+}
 
-pageTwo.addEventListener("click", function(event) {
-    pageTwo.style.backgroundColor = on
-    switch (position){
-        case 1:
-            pageOne.style.backgroundColor = off
-            context.style.animationName = 'oneToTwo'
-            break
-        case 2:
-            break
-        // case 3:
-        //     pageThree.style.backgroundColor = off
-        //     context.style.animationName = 'threeToTwo'
-        //     break
+const page_1 = () => {
+    pageOne.style.backgroundColor = on
+    if (position == 2){
+        pageTwo.style.backgroundColor = off
+        context.style.animationName = 'twoToOne'
+        position = 1
     }
-    position = 2
-})
+}
 
-// pageThree.addEventListener("click", function(event) {
-//     pageThree.style.backgroundColor = '#45C4B0'
-//     switch (position){
-//         case 1:
-//             pageOne.style.backgroundColor = off
-//             context.style.animationName = 'oneToThree'
-//             break
-//         case 2:
-//             pageTwo.style.backgroundColor = off
-//             context.style.animationName = 'twoToThree'
-//             break
-//         case 3:
-//             break
-//     }
-//     position = 3
-// })
+const page_2 = () => {
+    pageTwo.style.backgroundColor = on
+    if (position == 1){
+        pageOne.style.backgroundColor = off
+        context.style.animationName = 'oneToTwo'
+        position = 2
+    }
+}
+
+init()
