aef5dbaf7dfd6abaa2afef06b750af30a0e42a43
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-11-26 21:30:40 -0300

    code optimization scroll.js

--- JS/scroll.js
+++ JS/scroll.js
@@ -3,25 +3,32 @@ const elements = document.querySelectorAll('.content');
 
 const moved_page = () => {
     
-    const pos_elements = [].map.call(elements, (element) => {
-        return { 
-            element: element,
-            pos: element.getBoundingClientRect().y,
-            visible: false
-        };
-    })
-
-    const visibles = pos_elements.map((element) => {
-        if (element.pos < line_visibility && element.pos > 0){
-            return ({...element,  visible: true}) 
-        } else {
-            return element
-        }
-    })
-
-    const active_visible = visibles.find((element) => element.visible)
+    const elements_disabled = [].filter.call(elements, (element) => getComputedStyle(element).getPropertyValue("animation-play-state") == "paused")
+    // console.log(elements_disabled) 
     
-    active_visible.element.style.animationPlayState = "running"
+    if (elements_disabled.length) {
+        console.log("entrou")
+        const pos_elements = [].map.call(elements, (element) => {
+            return { 
+                element: element,
+                pos: element.getBoundingClientRect().y,
+                visible: false
+            };
+        })
+
+        const visibles = pos_elements.map((element) => {
+            if (element.pos < line_visibility && element.pos > 0){
+                return ({...element,  visible: true}) 
+            } else {
+                return element
+            }
+        })
+
+        const active_visible = visibles.find((element) => element.visible)
+
+        active_visible ? active_visible.element.style.animationPlayState = "running" : {}
+    }
+    
 }
 
 document.addEventListener('scroll', moved_page);
