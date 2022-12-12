209fb273bf2d8035d895ca276f43104047f7d8a0
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-12-11 21:18:29 -0300

    create config page

---
+++ JS/config_page.js
@@ -0,0 +1,19 @@
+const element_user = document.getElementById('user')
+const element_email = document.getElementById('email')
+const path = window.location.pathname
+
+const set_config = () => {
+    const user = localStorage.getItem("user") ?? 'aluno(a)'
+    const email = localStorage.getItem("email") ?? 'aluno@somas.org'
+    element_user ? element_user.value = user : {}
+    element_email ? element_email.value = email : {}
+}
+
+const send = () => {
+    localStorage.setItem('user', element_user.value)
+    localStorage.setItem('email', element_email.value)
+    
+    window.location.href = 'http://google.com'
+}
+
+set_config()
